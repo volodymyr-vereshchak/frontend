35b3e4d23116db45183d9013adfb47f2c077723e
Volodymyr Vereshchak <volodymyr.vereshchak@gmail.com>
2025-03-08 09:56:41 +0200

    added env

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-settings = {"BASE_API_URL": "fastapi_app", "API_PORT": 8000}
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+settings = {
+    "BASE_API_URL": os.getenv("BASE_API_URL"),
+    "API_PORT": os.getenv("API_PORT"),
+}
